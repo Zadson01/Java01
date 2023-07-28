65272349f7e6ba6c79d8de583360344d85f61b00
Zady01 <115594634+Zadson01@users.noreply.github.com>
2023-07-27 21:32:24 -0300

    Exercicios

---
+++ AtividaSaladeAula/Atividade03.java
@@ -0,0 +1,23 @@
+package AtividaSaladeAula;
+
+public class Atividade03 {
+
+	public static void main(String[] args) {
+	  
+		int a = 5;
+		int b = 6;
+		int c = 7;
+		int d = 8;
+		int valor01 = 0;
+		int valor02 = 0;
+		double valorfinal = 0;
+		
+		valor01 = a * b;
+		valor02 = c * d;
+		valorfinal = valor01 - valor02;
+		System.out.printf("Diferença %.0f",valorfinal);
+		
+
+	}
+
+}
